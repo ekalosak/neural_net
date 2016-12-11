e4c9bc5aff4eb0a5bc3091527cebe1b4e425124b
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2016-12-10 17:38:36 -0800

    compiling the net from symbolic Keras into Theano tensor representation, ready to train it

--- basic_example.py
+++ basic_example.py
@@ -1,7 +1,11 @@
 ### Straight from https://github.com/fchollet/keras
+# with some additional notes and visualization subroutines
 
-from keras.model import Sequential
+# Imports
+print("importing packages")
+from keras.models import Sequential
 from keras.layers import Dense, Activation
+import keras.utils.visualize_util as keras_vis
 
 # Use the sequential model to utilize a restricted class of tensor
 # representations of multilayer nn's - basically if the matrix is blocked
@@ -9,7 +13,37 @@ from keras.layers import Dense, Activation
 model = Sequential()
 
 # Initialize the simple example network architecture
-model.add(Dense(output_dim=64, input_dim=100))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=10))
-model.add(Activation("softmax"))
+# By the restrictions of the sequential model, each layer takes input only from
+# the previous and pipes output only to the next layer.
+# This particular sequential model has 100 input nodes fully connected (Dense)
+# to 64 hidden nodes and 10 output nodes.
+print("initializing sequential model")
+model.add(
+        Dense(
+            output_dim = 64,
+            input_dim = 100
+    ))
+model.add(
+        Activation("relu")
+    )
+model.add(
+        Dense(
+            output_dim = 10
+    ))
+model.add(
+        Activation("softmax")
+    )
+
+# Visualize the model before we compile it
+print("plotting basic model outline")
+keras_vis.plot(model, to_file='basic_dot_model.png')
+
+# Compile net
+print("compiling the net")
+model.compile(
+        loss='categorical_crossentropy',
+        optimizer='sgd',
+        metrics=['accuracy']
+    )
+
+# TODO load the MNIST dataset and train the net and visualize
